50a161b1d6a452a57ff231b0cc9d9d02dcc99508
V2-Shadow <vincent-kommerell@gmx.de>
2025-09-13 23:40:46 +0200

    Created Colors.sh and added test for all colors and also simple color links
    added colors and 2 flags to code just for fun

---
+++ colors.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+### Colors for Output ###
+nc='\e[0;m' # No Color
+blk='\e[1;30m' # Black
+red='\e[1;31m' # Red
+grn='\e[1;32m' # Green
+ylw='\e[1;33m' # Yellow
+blu='\e[1;34m' # Blue
+pur='\e[1;35m' # Purple
+cyn='\e[1;36m' # Cyan
+gry='\e[0;37m' # Light Gray
+wht='\e[1;37m' #White
+
+#Define Trans flag Colors just for fun :)
+#transblu='\e[38;2;91;206;250m'
+#transpnk='\e[38;2;245;169;184m'
+#transwht='\e[38;2;255;255;255m'
+#Set color to white for start
+text='\e[38;2;204;0;107m'
+border='\e[0;37m'
+
+echo -e "${wht}"
+echo -e "${red}ME??? lemme just..."
+echo -e "${border}###########################"
+trans='\e[38;2;245;169;184m'
+echo -e "${border}#${trans}####${text}####${trans}##${text}#${trans}#${text}####${trans}##${text}#${trans}######${border}#"
+trans='\e[38;2;91;206;250m'
+echo -e "${border}#${trans}###${text}#${trans}######${text}#${trans}#${text}#${trans}###${text}|${trans}#${text}#${trans}######${border}#"
+trans='\e[38;2;255;255;255m'
+echo -e "${border}#${trans}###${text}#${trans}##${text}##${trans}##${text}#${trans}#${text}####${trans}##${text}#${trans}######${border}#"
+trans='\e[38;2;91;206;250m'
+echo -e "${border}#${trans}###${text}#${trans}####${text}#${trans}#${text}#${trans}#${text}#${trans}##${text}#${trans}##${text}#${trans}######${border}#"
+trans='\e[38;2;91;206;250m'
+echo -e "${border}#${trans}####${text}####${trans}##${text}#${trans}#${text}#${trans}###${text}#${trans}#${text}####${trans}###${border}#"
+echo -e "${border}###########################"
+echo -e "${pur}Trump is a fascist"
+echo
+text='\e[48;2;90;24;154m'
+echo -e "${red}ME??? lemme just..."
+echo -e "${border}###########################"
+trans='\e[48;2;213;45;0m'
+echo -e "${border}#${trans}####${text}####${trans}##${text}#${trans}#${text}####${trans}##${text}#${trans}######${border}#"
+trans='\e[48;2;255;255;86m'
+echo -e "${border}#${trans}###${text}#${trans}######${text}#${trans}#${text}#${trans}###${text}|${trans}#${text}#${trans}######${border}#"
+trans='\e[48;2;255;255;255m'
+echo -e "${border}#${trans}###${text}#${trans}##${text}##${trans}##${text}#${trans}#${text}####${trans}##${text}#${trans}######${border}#"
+trans='\e[48;2;209;98;164m'
+echo -e "${border}#${trans}###${text}#${trans}####${text}#${trans}#${text}#${trans}#${text}#${trans}##${text}#${trans}##${text}#${trans}######${border}#"
+trans='\e[48;2;163;2;98m'
+echo -e "${border}#${trans}####${text}####${trans}##${text}#${trans}#${text}#${trans}###${text}#${trans}#${text}####${trans}###${border}#"
+echo -e "${border}###########################"
+
+
+echo
+echo -e "${nc} ######################"
+echo -e "${blk} ######################"
+echo -e "${gry} ######################"
+echo -e "${red} ######################"
+echo -e "${grn} ######################"
+echo -e "${ylw} ######################"
+echo -e "${blu} ######################"
+echo -e "${pur} ######################"
+echo -e "${cyn} ######################"
+echo -e "${wht} ######################"
+echo -e "${wht}"
